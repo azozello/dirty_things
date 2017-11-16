3fd39aa258df4213f2b41b788c1b2953012dab15
Azozello <deonis.panoff@gmail.com>
2017-11-16 18:34:09 +0100

    init commit

---
+++ src/main/java/com/example/DirtyThingsApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirtyThingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirtyThingsApplication.class, args);
+	}
+}
